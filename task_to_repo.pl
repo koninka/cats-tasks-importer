3db9186b66112749abed1bd10ea854072d6bcd28
Mark Tertishniy <mtertishniy@gmail.com>
2014-04-29 02:37:07 +1100

    Добавляю таблицу авторов и обработку уже существующих

--- task_to_repo.pl
+++ task_to_repo.pl
@@ -14,11 +14,14 @@ use Archive::Zip qw( :ERROR_CODES );
 # use XML::SAX;
 use Data::Dumper;
 use constant {
-   TMP_ZIP        => 'tmp_zip.zip',
-   REPOS_DIR      => 'tasks_rep',
-   TMP_ZIP_DIR    => 'dir',
-   PROBLEMS_DIR   => 'prs',
-   DEFAULT_AUTHOR => 'unknown'
+   TMP_ZIP         => 'tmp_zip.zip',
+   REPOS_DIR       => 'tasks_rep',
+   TMP_ZIP_DIR     => 'dir',
+   PROBLEMS_DIR    => 'problems',
+   KLENINS_EMAIL   => 'klenin@gmail.com',
+   DEFAULT_EMAIL   => 'unknown@example.com',
+   DEFAULT_AUTHOR  => 'Unknown Author',
+   EXTERNAL_AUTHOR => 'external'
 };
 
 Archive::Zip::setErrorHandler(sub {});
@@ -44,6 +47,68 @@ mkdir REPOS_DIR;
 #last commit sha1
 #git log -1 --pretty=format:%H
 # stat -c%y pr/problem_0OfMauOjAaOm6X7o3jHWpQoELt9wrn4K.zip | sed 's/^\([0-9\-]*\) \([0-9:]*\).*/\1 \2/'
+
+sub make_author_info {
+   my $h = {@_};
+   my $res = {email => (defined $h->{email} ? $h->{email} : DEFAULT_EMAIL)};
+   $res->{git_author} = $h->{git_author} if defined $h->{git_author};
+   return $res;
+}
+
+my %authors_map = (
+   'A. Klenin'           => make_author_info(email => KLENINS_EMAIL),
+   'А. Zhuplev'          => make_author_info(git_author => 'A. Zhuplev'),
+   'A. Zhuplev'          => make_author_info,
+   'A. Maksimov'         => make_author_info,
+   'Andrew Stankevich'   => make_author_info(git_author => 'A. Stankevich'),
+   'B. Vasilyev'         => make_author_info,
+   'D. Vikharev'         => make_author_info,
+   'E. Vasilyeva'        => make_author_info,
+   'Elena Kryuchkova'    => make_author_info(git_author => 'E. Kryuchkova'),
+   'Georgiy Korneev'     => make_author_info(git_author => 'G. Korneev'),
+   'I. Ludov'            => make_author_info,
+   'I. Tuphanov'         => make_author_info,
+   'I. Tufanov'          => make_author_info(git_author => 'I. Tuphanov'),
+   'I. Burago'           => make_author_info,
+   'Ludov I. Y.'         => make_author_info(git_author => 'I. Ludov'),
+   'Michail Mirzayanov'  => make_author_info(git_author => 'M. Mirzayanov'),
+   'Nick Durov'          => make_author_info(git_author => 'N. Durov'),
+   'T.Chistyakov'        => make_author_info,
+   'T. Chistyakov'       => make_author_info,
+   'Roman Elizarov'      => make_author_info(git_author => 'R. Elizarov'),
+   'А. Жуплев'           => make_author_info,
+   'А. Зенкина'          => make_author_info,
+   'А.Кленин'            => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
+   'А. Кленин'           => make_author_info(email => KLENINS_EMAIL),
+   'Александр С. Кленин' => make_author_info(email => KLENINS_EMAIL, git_author => 'A. Кленин'),
+   'А. Шавлюгин'         => make_author_info,
+   'В. Гринько'          => make_author_info,
+   'В. Кевролетин'       => make_author_info,
+   'В. Машенцев'         => make_author_info,
+   'В. Степанец'         => make_author_info,
+   'Г. Гренкин'          => make_author_info,
+   'Е. Васильева'        => make_author_info,
+   'Е. Иванова'          => make_author_info,
+   'И. Бураго'           => make_author_info,
+   'И. Лудов'            => make_author_info,
+   'И. Олейников'        => make_author_info,
+   'И. Туфанов'          => make_author_info,
+   'Кленин А.'           => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
+   'Кленин А.С.'         => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
+   'Кленина Н. В.'       => make_author_info(git_author => 'Н. Кленина'),
+   'М. Спорышев'         => make_author_info,
+   'Н.В. Кленина'        => make_author_info(git_author => 'Н. Кленина'),
+   'Н. В. Кленина'       => make_author_info(git_author => 'Н. Кленина'),
+   'Н. Кленина'          => make_author_info(git_author => 'Н. Кленина'),
+   'Н. Чистякова'        => make_author_info,
+   'О. Бабушкин'         => make_author_info,
+   'О.Ларькина'          => make_author_info(git_author => 'О. Ларькина'),
+   'О. Ларькина'         => make_author_info,
+   'О. Туфанов'          => make_author_info,
+   'С. Пак'              => make_author_info,
+   'C. Пак'              => make_author_info,
+   'Туфанов И.'          => make_author_info(git_author => 'И. Туфанов'),
+);
 
 my @authors = ();
 sub add_author {
@@ -98,10 +163,21 @@ foreach my $zip_name (@files) {
       my $xml = XML::LibXML->new()->parse_file($f);
       my $attributes = $xml->getDocumentElement()->getChildrenByTagName("Problem")->item(0)->attributes();
       my $title = $attributes->getNamedItem('title')->value if defined $attributes->getNamedItem('title');
-      my $author = $attributes->getNamedItem('author')->value if defined $attributes->getNamedItem('author');
-      $author = DEFAULT_AUTHOR if $author ~~ undef || $author eq "";
-      utf8::encode($author);
-      add_author($author);
+      $_ = $attributes->getNamedItem('author')->value if defined $attributes->getNamedItem('author');
+      $_ = DEFAULT_AUTHOR if $_ ~~ undef || $_ eq "";
+      utf8::encode($_);
+      add_author($_);
+      $_ = (split ',')[0];
+      s/\(.*\)//;
+      s/^\s*(.*?)\s*$/$1/;
+      my $author = $_;
+      if ($debug) {
+         if (exists $authors_map{$author}) {
+            print "Author $author exist\n";
+         } else {
+            print "Author $author don't exist\n";
+         }
+      }
       # if (!$needAuthorTable) {
       #    my $repo_path;
       #    if (exists($titles{$title})) {
