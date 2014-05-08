d3526a9580e03d4eafdb62023bfa745335e025db
Mark Tertishniy <mtertishniy@gmail.com>
2014-05-08 23:56:36 +1100

    Переношу таблицу авторов и константы к ним относящиеся в отдельный файл.

--- task_to_repo.pl
+++ task_to_repo.pl
@@ -9,21 +9,19 @@ use File::stat;
 use XML::LibXML;
 use Git::Repository;
 use Archive::Zip qw( :ERROR_CODES );
-# use XML::LibXML::Common;
-# use XML::NamespaceSupport;
-# use XML::SAX;
 use Data::Dumper;
 use constant {
    TMP_ZIP         => 'tmp_zip.zip',
+   XMLS_DIR        => 'task_xmls',
    REPOS_DIR       => 'tasks_rep',
    TMP_ZIP_DIR     => 'dir',
-   PROBLEMS_DIR    => 'problems',
-   KLENINS_EMAIL   => 'klenin@gmail.com',
-   DEFAULT_EMAIL   => 'unknown@example.com',
-   DEFAULT_AUTHOR  => 'Unknown Author',
-   EXTERNAL_AUTHOR => 'external'
+   PROBLEMS_DIR    => 'prs',
 };
 
+BEGIN { require "authors.pl"; }
+our (%authors_map);
+
+# git log --diff-filter=C -C -C -p -10 | grep ^[a-z]
 Archive::Zip::setErrorHandler(sub {});
 
 no if $] >= 5.018, 'warnings', "experimental::smartmatch";
@@ -33,82 +31,23 @@ my ($debug, $encodeToUTF8, $needAuthorTable) = undef, undef, undef;
 
 foreach (@ARGV) {
    tr/-//d;
-   $debug       = $_ eq 't' if !(defined $debug       && $debug);
+   $debug           = $_ eq 't' if !(defined $debug           && $debug);
    $encodeToUTF8    = $_ eq 'e' if !(defined $encodeToUTF8    && $encodeToUTF8);
    $needAuthorTable = $_ eq 'a' if !(defined $needAuthorTable && $needAuthorTable);
 }
-printf "debug started\n"                    if $debug;
+printf "debug started\n"                        if $debug;
 printf "recoding files enabled\n"               if $encodeToUTF8;
 printf "parse authors to authors.txt started\n" if $needAuthorTable;
 
+rmtree(XMLS_DIR);
+rmtree(TMP_ZIP_DIR);
 rmtree(REPOS_DIR);
 mkdir REPOS_DIR;
+mkdir XMLS_DIR;
 
 #last commit sha1
 #git log -1 --pretty=format:%H
 # stat -c%y pr/problem_0OfMauOjAaOm6X7o3jHWpQoELt9wrn4K.zip | sed 's/^\([0-9\-]*\) \([0-9:]*\).*/\1 \2/'
-
-sub make_author_info {
-   my $h = {@_};
-   my $res = {email => (defined $h->{email} ? $h->{email} : DEFAULT_EMAIL)};
-   $res->{git_author} = $h->{git_author} if defined $h->{git_author};
-   return $res;
-}
-
-my %authors_map = (
-   'A. Klenin'           => make_author_info(email => KLENINS_EMAIL),
-   'А. Zhuplev'          => make_author_info(git_author => 'A. Zhuplev'),
-   'A. Zhuplev'          => make_author_info,
-   'A. Maksimov'         => make_author_info,
-   'Andrew Stankevich'   => make_author_info(git_author => 'A. Stankevich'),
-   'B. Vasilyev'         => make_author_info,
-   'D. Vikharev'         => make_author_info,
-   'E. Vasilyeva'        => make_author_info,
-   'Elena Kryuchkova'    => make_author_info(git_author => 'E. Kryuchkova'),
-   'Georgiy Korneev'     => make_author_info(git_author => 'G. Korneev'),
-   'I. Ludov'            => make_author_info,
-   'I. Tuphanov'         => make_author_info,
-   'I. Tufanov'          => make_author_info(git_author => 'I. Tuphanov'),
-   'I. Burago'           => make_author_info,
-   'Ludov I. Y.'         => make_author_info(git_author => 'I. Ludov'),
-   'Michail Mirzayanov'  => make_author_info(git_author => 'M. Mirzayanov'),
-   'Nick Durov'          => make_author_info(git_author => 'N. Durov'),
-   'T.Chistyakov'        => make_author_info,
-   'T. Chistyakov'       => make_author_info,
-   'Roman Elizarov'      => make_author_info(git_author => 'R. Elizarov'),
-   'А. Жуплев'           => make_author_info,
-   'А. Зенкина'          => make_author_info,
-   'А.Кленин'            => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
-   'А. Кленин'           => make_author_info(email => KLENINS_EMAIL),
-   'Александр С. Кленин' => make_author_info(email => KLENINS_EMAIL, git_author => 'A. Кленин'),
-   'А. Шавлюгин'         => make_author_info,
-   'В. Гринько'          => make_author_info,
-   'В. Кевролетин'       => make_author_info,
-   'В. Машенцев'         => make_author_info,
-   'В. Степанец'         => make_author_info,
-   'Г. Гренкин'          => make_author_info,
-   'Е. Васильева'        => make_author_info,
-   'Е. Иванова'          => make_author_info,
-   'И. Бураго'           => make_author_info,
-   'И. Лудов'            => make_author_info,
-   'И. Олейников'        => make_author_info,
-   'И. Туфанов'          => make_author_info,
-   'Кленин А.'           => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
-   'Кленин А.С.'         => make_author_info(email => KLENINS_EMAIL, git_author => 'А. Кленин'),
-   'Кленина Н. В.'       => make_author_info(git_author => 'Н. Кленина'),
-   'М. Спорышев'         => make_author_info,
-   'Н.В. Кленина'        => make_author_info(git_author => 'Н. Кленина'),
-   'Н. В. Кленина'       => make_author_info(git_author => 'Н. Кленина'),
-   'Н. Кленина'          => make_author_info(git_author => 'Н. Кленина'),
-   'Н. Чистякова'        => make_author_info,
-   'О. Бабушкин'         => make_author_info,
-   'О.Ларькина'          => make_author_info(git_author => 'О. Ларькина'),
-   'О. Ларькина'         => make_author_info,
-   'О. Туфанов'          => make_author_info,
-   'С. Пак'              => make_author_info,
-   'C. Пак'              => make_author_info,
-   'Туфанов И.'          => make_author_info(git_author => 'И. Туфанов'),
-);
 
 my @authors = ();
 sub add_author {
@@ -147,6 +86,46 @@ my @files = sort{$files{$a} <=> $files{$b}} keys %files;
 #    print "$keys\t", scalar localtime($files{$keys}), "\n";
 # }
 
+# my $xmls_dir = XMLS_DIR;
+# Git::Repository->run(init => $xmls_dir);
+# my $xml_repo = Git::Repository->new(
+#    work_tree => $xmls_dir,
+#    {
+#       env => {
+#          GIT_AUTHOR_NAME  => DEFAULT_AUTHOR,
+#          GIT_AUTHOR_EMAIL => DEFAULT_EMAIL
+#       }
+#    }
+# );
+# foreach my $zip_name (@files) {
+#    print "zip = $zip_name, \n" if $debug;
+#    rmtree(TMP_ZIP_DIR);
+#    eval {
+#       eval {
+#          extract_zip("$dir/$zip_name");
+#       };
+#       if ($@) {
+#          unlink $tmp_zip;
+#          `echo "y" | zip -F $dir/$zip_name --out $tmp_zip`;
+#          extract_zip($tmp_zip);
+#       }
+#       my ($f) = glob('dir/*.xml');
+#       copy $f, $xmls_dir;
+#       $xml_repo->run(add => '.');
+#       $xml_repo->run(commit => '-m', "Add $f");
+#    };
+#    if ($@) {
+#       push @failed_zips, {zip => $zip_name, msg => $error};
+#    }
+# }
+
+# print "=======failed zips=======\n";
+# foreach (@failed_zips) {
+#    print "$_->{zip} - $_->{msg}\n";
+# }
+
+# exit;
+
 foreach my $zip_name (@files) {
    print "zip = $zip_name, \n" if $debug;
    rmtree(TMP_ZIP_DIR);
@@ -178,39 +157,40 @@ foreach my $zip_name (@files) {
             print "Author $author don't exist\n";
          }
       }
-      # if (!$needAuthorTable) {
-      #    my $repo_path;
-      #    if (exists($titles{$title})) {
-      #       $repo_path = $titles{$title};
-      #    } else {
-      #       $titles{$title} = $repo_path = "$repos_dir/$1" if $f =~ m|dir/(.*).xml|;
-      #       mkdir $repo_path;
-      #       Git::Repository->run(init => $repo_path);
-      #    }
-      #    print "author = $author\n" if $test_mode;
-      #    my $repo = Git::Repository->new(
-      #       work_tree => $repo_path,
-      #       {
-      #          env => {
-      #             GIT_AUTHOR_NAME  => $author,
-      #             GIT_AUTHOR_EMAIL => 'some@mail.ru'
-      #          }
-      #       }
-      #    );
-      #    foreach (glob('dir/*')){
-      #       copy $_, $repo_path;
-      #    }
-      #    $repo->run(add => '.');
-      #    $repo->run(commit => '-m', 'Initial commit', sprintf("--date=%s", stat("$dir/$zip_name")->mtime));
-      # }
-      #===================================================
-      # print scalar localtime stat("$dir/$zip_name")->mtime;
-      # print "\n";
-      # print `stat -c%y $dir/$zip_name`;
-      # print "\n";
-      # last;
-      # $task_zip =~ s|problems/||;
-      # printf "%s\n", $task_zip;
+      if (!$needAuthorTable) {
+         my $repo_path;
+         my $commit_msg = 'Initial commit';
+         if (exists($titles{$title})) {
+            $repo_path = $titles{$title};
+            $commit_msg = 'Change task';
+         } else {
+            $zip_name =~ m|^problem_(.*)\.zip$|;
+            $titles{$title} = $repo_path = "$repos_dir/$1";
+            mkdir $repo_path;
+            Git::Repository->run(init => $repo_path);
+         }
+         my $repo = Git::Repository->new(
+            work_tree => $repo_path,
+            {
+               env => {
+                  GIT_AUTHOR_NAME  =>
+                        exists $authors_map{$author}
+                     ?  (
+                           exists $authors_map{$author}{git_author}
+                         ? $authors_map{$author}{git_author}
+                         : $author
+                        )
+                     : EXTERNAL_AUTHOR,
+                  GIT_AUTHOR_EMAIL => exists $authors_map{$author} ? $authors_map{$author}{email} : DEFAULT_EMAIL
+               }
+            }
+         );
+         foreach (glob("$tmp_dir/*")){
+            copy $_, $repo_path;
+         }
+         $repo->run(add => '.');
+         $repo->run(commit => '-m', $commit_msg, sprintf("--date=%s", stat("$dir/$zip_name")->mtime));
+      }
    };
    if ($@) {
       push @failed_zips, {zip => $zip_name, msg => $error};
@@ -228,8 +208,10 @@ sub print_res {
    }
 }
 
-@authors = sort @authors;
-print_res("authors name", \@authors);
+if ($needAuthorTable) {
+   @authors = sort @authors;
+   print_res("authors name", \@authors);
+}
 
 open FILE, ">authors.txt" or die $!;
 foreach (@authors) {
