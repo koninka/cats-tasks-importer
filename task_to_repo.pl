31f0109a2fd50c4b4dc88a6f33c8ccdbe98a5880
Mark Tertishniy <mtertishniy@gmail.com>
2014-05-14 15:08:18 +1100

    Большие изменения
    Добавлено определение переименований и т.д.(Лень писать)

--- task_to_repo.pl
+++ task_to_repo.pl
@@ -7,52 +7,48 @@ BEGIN {
 
 use strict;
 use warnings;
-# use diagnostics;
+use lib CATS_DB;
+use CATS::DB;
 use File::Path;
 use File::Copy;
 use File::stat;
 use XML::LibXML;
 use Git::Repository;
 use Archive::Zip qw( :ERROR_CODES );
+use constant LIST_PROCESSING => 'list_proc.txt';
 use Data::Dumper;
-use constant {
-   TMP_ZIP         => 'tmp_zip.zip',
-   XMLS_DIR        => 'task_xmls',
-   REPOS_DIR       => 'tasks_rep',
-   TMP_ZIP_DIR     => 'dir',
-   PROBLEMS_DIR    => 'prs',
-};
 
-BEGIN { require "authors.pl"; }
-our (%authors_map);
+our %authors_map;
 
-# git log --diff-filter=C -C -C -p -10 | grep ^[a-z]
 Archive::Zip::setErrorHandler(sub {});
 
 no if $] >= 5.018, 'warnings', "experimental::smartmatch";
 
-#get vars
-my ($debug, $encodeToUTF8, $needAuthorTable) = undef, undef, undef;
+#-----------------------------------------------------------------
+#-----------------SCRIPT OPTIONS AND PREPARATION------------------
+#-----------------------------------------------------------------
+my ($DEBUG, $encodeToUTF8, $needAuthorTable, $proceedHandle) = undef, undef, undef, undef;
 
 foreach (@ARGV) {
    tr/-//d;
-   $debug           = $_ eq 't' if !(defined $debug           && $debug);
+   $DEBUG           = $_ eq 't' if !(defined $DEBUG           && $DEBUG);
    $encodeToUTF8    = $_ eq 'e' if !(defined $encodeToUTF8    && $encodeToUTF8);
+   $proceedHandle   = $_ eq 'p' if !(defined $proceedHandle   && $proceedHandle);
    $needAuthorTable = $_ eq 'a' if !(defined $needAuthorTable && $needAuthorTable);
 }
-printf "debug started\n"                        if $debug;
+printf "DEBUG STARTED\n"                        if $DEBUG;
 printf "recoding files enabled\n"               if $encodeToUTF8;
-printf "parse authors to authors.txt started\n" if $needAuthorTable;
+printf "PARSE AUTHORS TO authors.txt STARDER\n" if $needAuthorTable;
+if ($proceedHandle) {
+   print "continue files processing\n";
+} else {
+   print "start from the beginning files processing\n";
+   rmtree REPOS_DIR;
+}
 
-rmtree(XMLS_DIR);
-rmtree(TMP_ZIP_DIR);
-rmtree(REPOS_DIR);
-mkdir REPOS_DIR;
-mkdir XMLS_DIR;
-
-#last commit sha1
-#git log -1 --pretty=format:%H
-# stat -c%y pr/problem_0OfMauOjAaOm6X7o3jHWpQoELt9wrn4K.zip | sed 's/^\([0-9\-]*\) \([0-9:]*\).*/\1 \2/'
+rmtree XMLS_DIR;
+rmtree TMP_ZIP_DIR;
+unlink TMP_ZIP;
 
 my @authors = ();
 sub add_author {
@@ -67,164 +63,201 @@ sub extract_zip {
    my @xml_members = $zip->membersMatching('.*\.xml$');
    error('*.xml not found') if !@xml_members;
    error('found several *.xml in archive') if @xml_members > 1;
-   $zip->extractTree('', 'dir/') == AZ_OK or error("can't extract");
+   $zip->extractTree('', TMP_ZIP_DIR) == AZ_OK or error("can't extract");
 }
 
-my $error;
-sub error {
-   $error = $_[0];
-   die;
-}
+my %titles_id = ();
+my %id_titles = ();
 
-my $dir = PROBLEMS_DIR;
-my $tmp_zip = TMP_ZIP;
-my $tmp_dir = TMP_ZIP_DIR;
-my $repos_dir = REPOS_DIR;
-my %tasks = ();
-my %titles = ();
-my @failed_zips = ();
+my %zip_files = map {m|@{[PROBLEMS_DIR]}/(.*)|; $1 => stat($_)->mtime} glob(PROBLEMS_DIR . '/*.zip');
+my @zip_files = sort{$zip_files{$a} <=> $zip_files{$b}} keys %zip_files;
 
-my %files = map {m|$dir/(.*)|; $1 => stat($_)->mtime} glob("$dir/*.zip");
-my @files = sort{$files{$a} <=> $files{$b}} keys %files;
+#-----------------------------------------------------------------
+#----------------------RENAMES DETERMINATION----------------------
+#-----------------------------------------------------------------
+goto REPOSITORY_CREATION if $needAuthorTable;
 
-# foreach my $keys (sort{$files{$a} <=> $files{$b}} keys %files) {
-#    print "$keys\t", scalar localtime($files{$keys}), "\n";
-# }
-
-# my $xmls_dir = XMLS_DIR;
-# Git::Repository->run(init => $xmls_dir);
-# my $xml_repo = Git::Repository->new(
-#    work_tree => $xmls_dir,
-#    {
-#       env => {
-#          GIT_AUTHOR_NAME  => DEFAULT_AUTHOR,
-#          GIT_AUTHOR_EMAIL => DEFAULT_EMAIL
-#       }
-#    }
-# );
-# foreach my $zip_name (@files) {
-#    print "zip = $zip_name, \n" if $debug;
-#    rmtree(TMP_ZIP_DIR);
-#    eval {
-#       eval {
-#          extract_zip("$dir/$zip_name");
-#       };
-#       if ($@) {
-#          unlink $tmp_zip;
-#          `echo "y" | zip -F $dir/$zip_name --out $tmp_zip`;
-#          extract_zip($tmp_zip);
-#       }
-#       my ($f) = glob('dir/*.xml');
-#       copy $f, $xmls_dir;
-#       $xml_repo->run(add => '.');
-#       $xml_repo->run(commit => '-m', "Add $f");
-#    };
-#    if ($@) {
-#       push @failed_zips, {zip => $zip_name, msg => $error};
-#    }
-# }
-
-# print "=======failed zips=======\n";
-# foreach (@failed_zips) {
-#    print "$_->{zip} - $_->{msg}\n";
-# }
-
-# exit;
-
-foreach my $zip_name (@files) {
-   print "zip = $zip_name, \n" if $debug;
-   rmtree(TMP_ZIP_DIR);
+mkdir XMLS_DIR;
+Git::Repository->run(init => XMLS_DIR);
+my $xml_repo = Git::Repository->new(
+   work_tree => XMLS_DIR,
+   {
+      env => {
+         GIT_AUTHOR_NAME  => DEFAULT_AUTHOR,
+         GIT_AUTHOR_EMAIL => DEFAULT_EMAIL
+      }
+   }
+);
+CATS::DB::sql_connect;
+my $sth = $dbh->prepare('SELECT id FROM problems WHERE title=? ORDER BY id');
+open FILE, ">zip_xml.txt" or die $! if $DEBUG;
+foreach my $zip (@zip_files) {
+   my $zip_path = PROBLEMS_DIR . "/$zip";
    eval {
       eval {
-         extract_zip("$dir/$zip_name");
+         extract_zip($zip_path);
       };
       if ($@) {
-         unlink $tmp_zip;
-         `echo "y" | zip -F $dir/$zip_name --out $tmp_zip`;
-         extract_zip($tmp_zip);
+         `echo "y" | zip -F $zip_path --out @{[TMP_ZIP]}`;
+         extract_zip(TMP_ZIP);
+         unlink TMP_ZIP;
       }
-      my ($f) = glob('dir/*.xml');
-      my $xml = XML::LibXML->new()->parse_file($f);
-      my $attributes = $xml->getDocumentElement()->getChildrenByTagName("Problem")->item(0)->attributes();
-      my $title = $attributes->getNamedItem('title')->value if defined $attributes->getNamedItem('title');
-      $_ = $attributes->getNamedItem('author')->value if defined $attributes->getNamedItem('author');
-      $_ = DEFAULT_AUTHOR if $_ ~~ undef || $_ eq "";
-      utf8::encode($_);
+      my ($xml_file) = glob(TMP_ZIP_DIR . '/*.xml');
+      my ($el) = XML::LibXML->load_xml(location => $xml_file)->getDocumentElement()->getElementsByTagName('Problem');
+      my $title = $el->getAttribute('title');
+      # utf8::encode($title);
+      unless (exists $titles_id{$title}) {
+         $sth->bind_param(1, $title);
+         $sth->execute;
+         my $aref = $sth->fetchall_arrayref;
+         error("Task's record for $title doesn't exist in the database") if !@$aref;
+         $titles_id{$title} = $aref->[0][0];
+         $id_titles{$titles_id{$title}} = $title;
+         print FILE "$zip $titles_id{$title}\n" if $DEBUG;
+         copy $xml_file, "@{[XMLS_DIR]}/$titles_id{$title}.xml";
+         $xml_repo->run(add => '.');
+         $xml_repo->run(commit => '-m', "Add $titles_id{$title}");
+         $dbh->commit;
+         error("There is more than one record for $title") if @$aref > 1;
+      }
+   };
+   if ($@) {
+      add_failed_zip($zip);
+   } else {
+      rmtree TMP_ZIP_DIR;
+   }
+}
+close FILE if $DEBUG;
+$sth->finish;
+CATS::DB::sql_disconnect;
+
+print_failed_zips;
+
+my @log = $xml_repo->run(log => '--diff-filter=C', '-C', "-C@{[SIMILARITY_INDEX]}%", '--summary', '--format="% "');
+@log = reverse map {m/([0-9]+)\.xml => ([0-9]+)\.xml \(([0-9]+)%\)/; {old_name => $1, new_name => $2, sidx => $3}} grep {/^ copy/} @log;
+
+my %renamings = ();    #choose from several alternatives that renaming which has the highest similarity index
+for my $i (0..$#log) {
+   my $h = $log[$i];
+   for my $j ($i+1..$#log) {
+      if (($log[$i]->{old_name} eq $log[$j]->{old_name}) && ($log[$i]->{sidx} < $log[$j]->{sidx})) {
+         $h = $log[$j];
+         $log[$i] = $log[$j];
+      }
+   }
+   $renamings{$h->{new_name}} = $h->{old_name};
+}
+
+if ($DEBUG) {
+   print "$_ <= $renamings{$_}\n" foreach keys %renamings;
+}
+
+my @base_tasks_to_be_renamed = ();
+foreach (keys %renamings) {
+   $_ = $renamings{$_} while defined $renamings{$_};
+   next if $_ ~~ @base_tasks_to_be_renamed;
+   push @base_tasks_to_be_renamed, $_;
+}
+
+#-----------------------------------------------------------------
+#------------------REPOSITORY CREATION FOR TASKS------------------
+#-----------------------------------------------------------------
+REPOSITORY_CREATION:
+mkdir REPOS_DIR if !$needAuthorTable;
+my @reps = ();
+my @renamed_tasks = ();
+foreach my $zip (@zip_files) {
+   my $zip_path = PROBLEMS_DIR . "/$zip";
+   # print "handle $zip\n" if $DEBUG;
+   # eval {
+      eval {
+         extract_zip($zip_path);
+      };
+      if ($@) {
+         `echo "y" | zip -F $zip_path --out @{[TMP_ZIP]}`;
+         extract_zip(TMP_ZIP);
+         unlink TMP_ZIP;
+      }
+      my ($xml_file) = glob(TMP_ZIP_DIR . '/*.xml');
+      my ($el) = XML::LibXML->load_xml(location => $xml_file)->getDocumentElement()->getElementsByTagName('Problem');
+      my $title = $el->getAttribute('title');
+      # utf8::encode($title);
+      $_ = $el->getAttribute('author') if defined $el->getAttribute('author');
+      $_ = DEFAULT_AUTHOR if $_ ~~ undef || $_ eq '';
+      # utf8::encode($_);
       add_author($_);
       $_ = (split ',')[0];
       s/\(.*\)//;
       s/^\s*(.*?)\s*$/$1/;
       my $author = $_;
-      if ($debug) {
-         if (exists $authors_map{$author}) {
-            print "Author $author exist\n";
-         } else {
-            print "Author $author don't exist\n";
+      goto END_EVAL if $needAuthorTable || !(exists $titles_id{$title} && defined $titles_id{$title});
+      my $task_id = $titles_id{$title};
+      my $repo_path = "@{[REPOS_DIR]}/$task_id";
+      my $commit_msg = 'Change task';
+      my $xml_id = $task_id;
+      my $already_has_rep = 0;
+      if (exists $renamings{$task_id}) {
+         $already_has_rep = 1;
+         unless ($task_id ~~ @renamed_tasks) {
+            push @renamed_tasks, $task_id;
+            $commit_msg = "Rename task from '$id_titles{$renamings{$task_id}}' to '$id_titles{$task_id}'";
          }
+         $task_id = $renamings{$task_id} while defined $renamings{$task_id};
+         $repo_path = "@{[REPOS_DIR]}/$task_id";
+      } elsif ($task_id ~~ @reps) {
+         $already_has_rep = $task_id ~~ @base_tasks_to_be_renamed;
+      } else {
+         $commit_msg = 'Initial commit';
+         push @reps, $task_id;
+         mkdir $repo_path;
+         Git::Repository->run(init => $repo_path);
       }
-      if (!$needAuthorTable) {
-         my $repo_path;
-         my $commit_msg = 'Initial commit';
-         if (exists($titles{$title})) {
-            $repo_path = $titles{$title};
-            $commit_msg = 'Change task';
-         } else {
-            $zip_name =~ m|^problem_(.*)\.zip$|;
-            $titles{$title} = $repo_path = "$repos_dir/$1";
-            mkdir $repo_path;
-            Git::Repository->run(init => $repo_path);
-         }
-         my $repo = Git::Repository->new(
-            work_tree => $repo_path,
-            {
-               env => {
-                  GIT_AUTHOR_NAME  =>
-                        exists $authors_map{$author}
-                     ?  (
-                           exists $authors_map{$author}{git_author}
-                         ? $authors_map{$author}{git_author}
-                         : $author
-                        )
-                     : EXTERNAL_AUTHOR,
-                  GIT_AUTHOR_EMAIL => exists $authors_map{$author} ? $authors_map{$author}{email} : DEFAULT_EMAIL
-               }
+      my $repo = Git::Repository->new(
+         work_tree => $repo_path,
+         {
+            env => {
+               GIT_AUTHOR_NAME  =>
+                     exists $authors_map{$author}
+                  ?  (
+                        exists $authors_map{$author}{git_author}
+                      ? $authors_map{$author}{git_author}
+                      : $author
+                     )
+                  : EXTERNAL_AUTHOR,
+               GIT_AUTHOR_EMAIL => exists $authors_map{$author} ? $authors_map{$author}{email} : DEFAULT_EMAIL
             }
-         );
-         foreach (glob("$tmp_dir/*")){
-            copy $_, $repo_path;
          }
-         $repo->run(add => '.');
-         $repo->run(commit => '-m', $commit_msg, sprintf("--date=%s", stat("$dir/$zip_name")->mtime));
+      );
+      $repo->run(rm => '*', '--ignore-unmatch');
+      # $repo->run(rm => '*.xml') if $already_has_rep;
+      foreach (glob(TMP_ZIP_DIR . '/*')) {
+         # if ($_ eq $xml_file) {
+            # copy $_, "$repo_path/${xml_id}.xml";
+         # } else {
+            copy $_, $repo_path;
+         # }
       }
-   };
+      $repo->run(add => '-A');
+      $repo->run(commit => '-m', $commit_msg, sprintf('--date=%s', stat($zip_path)->mtime));
+      END_EVAL:
+   # };
    if ($@) {
-      push @failed_zips, {zip => $zip_name, msg => $error};
+      print "$@\n";
+      add_failed_zip($zip);
+   } else {
+      rmtree TMP_ZIP_DIR;
    }
 }
 
-sub print_res {
-   if (scalar @{$_[1]} > 0) {
-      $, = "\n";
-      print "=======$_[0]=======\n";
-      print @{$_[1]};
-      print "\n";
-   } else {
-      print "$_[0] is empty\n";
-   }
-}
+# foreach (@authors) {
+#    print "true - $_\n" if utf8::is_utf8($_);
+# }
 
 if ($needAuthorTable) {
-   @authors = sort @authors;
-   print_res("authors name", \@authors);
-}
-
-open FILE, ">authors.txt" or die $!;
-foreach (@authors) {
-   print FILE "$_\n";
-}
-close FILE;
-
-print "=======failed zips=======\n";
-foreach (@failed_zips) {
-   print "$_->{zip} - $_->{msg}\n";
+   open FILE, '>authors.txt' or die $!;
+   foreach (sort @authors) {
+      utf8::encode($_);
+      print FILE "$_\n";
+   }
+   close FILE;
 }
