49e7fe0f6f36a0b714323d6dcef0c3db94549246
Mark Tertishniy <mtertishniy@gmail.com>
2014-05-19 01:39:20 +1100

    Новый подход(построение графа истории)

--- task_to_repo.pl
+++ task_to_repo.pl
@@ -17,7 +17,8 @@ use Digest::SHA qw(sha1_hex);
 use Git::Repository;
 use Archive::Zip qw( :ERROR_CODES );
 use constant {
-   LOG_FILE       =>  'log',
+   LOG_FILE =>  'log',
+   ERROR_V_DEL =>'----------------------------------------------------------------------------------------------------------------------------',
    LIST_PROCESSING => 'list_proc.txt'
 };
 use Data::Dumper;
@@ -81,14 +82,14 @@ my @zip_files = sort{$zip_files{$a} <=> $zip_files{$b}} keys %zip_files;
 #-----------------------------------------------------------------
 #----------------------------FIX ZIPS-----------------------------
 #-----------------------------------------------------------------
-my %fixed_zips = ();
-foreach my $zip_name (@zip_files) {
-   my $zip_path = PROBLEMS_DIR . "/$zip_name";
-   if (Archive::Zip->new()->read($zip_path) != AZ_OK) {
-      set_error("can't read");
-      add_failed_zip($zip_name);
-   }
-}
+# my %fixed_zips = ();
+# foreach my $zip_name (@zip_files) {
+#    my $zip_path = PROBLEMS_DIR . "/$zip_name";
+#    if (Archive::Zip->new()->read($zip_path) != AZ_OK) {
+#       set_error("can't read");
+#       add_failed_zip($zip_name);
+#    }
+# }
 
 # my %fixed_zips = ();
 # foreach my $zip_name (@zip_files) {
@@ -110,9 +111,64 @@ foreach my $zip_name (@zip_files) {
 # }
 
 #-----------------------------------------------------------------
-#----------------------RENAMES DETERMINATION----------------------
+#----------HISTORY CREATION (WITH RENAMES DETERMINATION)----------
 #-----------------------------------------------------------------
 goto REPOSITORY_CREATION if $needAuthorTable;
+my %edges = ();
+my %sha_zip = ();
+my %zip_sha = ();
+my @start_v = ();
+my %reverse_renamings = ();
+
+sub add_to_sha_zip {
+   my ($zip, $sha) = @_;
+   if (defined $sha_zip{$sha} && @{$sha_zip{$sha}} > 0) {
+      push @{$sha_zip{$sha}}, $zip;
+   } else {
+      $sha_zip{$sha} = [$zip];
+   }
+   $zip_sha{$zip} = $sha;
+}
+
+sub add_start_v {
+   my ($zip, $sha) = @_;
+   push @start_v, {zip => $zip, title => $sha};
+   add_to_sha_zip($zip, $sha);
+}
+
+sub get_leaf {
+   (my $zip) = @_;
+   $zip = $edges{$zip}{zip} while exists $edges{$zip}{zip};
+   return $zip;
+}
+
+sub good_add_edge {
+   (my $zip, my $sha1, my $sha2) = @_;
+   my $idx;
+   for (my $i = 0; $i < @{$sha_zip{$sha1}}; $i++) {
+      my $leaf = get_leaf($sha_zip{$sha1}[$i]);
+      my $isExist = 0;
+      if ($sha1 ne $sha2) {
+         my $tmp_sha = $sha1;
+         while (!$isExist && exists $reverse_renamings{$tmp_sha}) {
+            $tmp_sha = $reverse_renamings{$tmp_sha};
+            $isExist = $sha2 eq $tmp_sha;
+         }
+      }
+      next if $isExist;
+      if ($zip_sha{$leaf} eq $sha1) {
+         $edges{$leaf} = {zip => $zip, title => $sha2};
+         $idx = $i;
+         last;
+      }
+   }
+   $sha_zip{$sha1}[$idx] = $zip if $sha1 eq $sha2 && defined $idx;
+   if ($sha1 ne $sha2 && defined $idx) {
+      $reverse_renamings{$sha2} = $sha1 if !exists $reverse_renamings{$sha2};
+   }
+   add_start_v($zip, $sha2) if $idx ~~ undef;
+   $zip_sha{$zip} = $sha2;
+}
 
 mkdir XMLS_DIR;
 Git::Repository->run(init => XMLS_DIR);
@@ -125,16 +181,10 @@ my $xml_repo = Git::Repository->new(
       }
    }
 );
-my @start_v = ();
-my %edges = ();
-my %sha_zip = ();
-# CATS::DB::sql_connect;
-# my $sth = $dbh->prepare('SELECT id FROM problems WHERE title=? ORDER BY id');
-open FILE, ">zip_xml.txt" or die $! if $DEBUG;
 foreach my $zip (@zip_files) {
    next if exist_failed_zip($zip);
    my $zip_path = PROBLEMS_DIR . "/$zip";
-   # eval {
+   eval {
       extract_zip($zip_path);
       my ($xml_file) = glob(TMP_ZIP_DIR . '/*.xml');
       my $xml;
@@ -143,67 +193,158 @@ foreach my $zip (@zip_files) {
       my ($el) = $xml->getDocumentElement()->getElementsByTagName('Problem');
       my $title = $el->getAttribute('title');
       utf8::encode($title);
+      # my $sha1 = $title;
       my $sha1 = sha1_hex($title);
-      print "$sha1 $zip\n";
       if (-e XMLS_DIR . "/$sha1.xml") {
-         $xml_repo->run(rm => "$sha1.xml", '--ignore-unmatch');
          copy $xml_file, XMLS_DIR . "/$sha1.xml";
          $xml_repo->run(add => '.');
-         $xml_repo->run(commit => '-m', "update '$title'");
-         $edges{$sha_zip{$sha1}} = $zip;
+         $xml_repo->run(commit => '-m', "update '$title', zip - $zip");
+         good_add_edge($zip, $sha1, $sha1);
       } else {
          copy $xml_file, XMLS_DIR . "/$sha1.xml";
          $xml_repo->run(add => '.');
-         $xml_repo->run(commit => '-m', "add '$title'");
-         my @log = $xml_repo->run(log => '--diff-filter=C', '-C', "-C@{[SIMILARITY_INDEX]}%", '--summary', '--format="% "');
-         @log = map {m/^\s+copy (.*)\.xml => (.*)\.xml \(([0-9]+)%\)/; {old_name => $1, new_name => $2, sidx => $3}} grep {/^ copy/} @log;
-         if (@log > 0) {
-            my ($desc) = @log;
-            my $sha_commmit = $xml_repo->run(rev-parse => 'HEAD');
-            $xml_repo->run(mv)
-            print "$desc->{old_name}.xml !!\n";
+         $xml_repo->run(commit => '-m', "add '$title', zip - $zip");
+         my @log = $xml_repo->run(log => '--diff-filter=C', '-C', "-C@{[SIMILARITY_INDEX]}%", '--summary', '--format="% "', '-1');
+         my ($tmp_str) = @log = grep {/^ copy/} @log;
+         my ($desc) = map {m/^\s+copy (.*)\.xml => (.*)\.xml \(([0-9]+)%\)/; {old_name => $1, new_name => $2}} @log;
+         if (defined $desc && -e XMLS_DIR . "/$desc->{old_name}.xml") {
             $xml_repo->run(rm => "$desc->{old_name}.xml");
             $xml_repo->run(commit => '-m', "delete old version of '$title'");
-            $edges{$sha_zip{$desc->{old_name}}} = $zip;
+            if ($desc->{new_name} ne $sha1) {
+               print "error with renames DETERMINATION\n";
+               exit;
+            }
+            good_add_edge($zip, $desc->{old_name}, $desc->{new_name});
+            add_to_sha_zip($zip, $desc->{new_name});
+         } else {
+            add_start_v($zip, $sha1);
          }
-         if (@log > 1) {
-            print Dumper(@log);
-            print "\n";
-            print $zip;
-            print "\n";
-         }
-         # $, = "\n";
-         # print "+++++++\n";
-         # print Dumper(@log);
-         # print "\n";
-         # exit if @log > 1;
       }
-      $sha_zip{$sha1} = $zip;
-      # $sth->bind_param(1, $title);
-      # $sth->execute;
-      # my $aref = $sth->fetchall_arrayref;
-      # error("Task's record for '$title' doesn't exist in the database") if !@$aref;
-      # $titles_id{$title} = $aref->[0][0];
-      # $id_titles{$titles_id{$title}} = $title;
-      # print FILE "$zip $titles_id{$title}\n" if $DEBUG;
-      # copy $xml_file, "@{[XMLS_DIR]}/$titles_id{$title}.xml";
-      # $xml_repo->run(add => '.');
-      # $xml_repo->run(commit => '-m', "Add $titles_id{$title}");
-      # $dbh->commit;
-      # error("There is more than one record for '$title'") if @$aref > 1;
-   # };
-   if ($@) {
-      add_failed_zip($zip);
-   }
+   };
+   add_failed_zip($zip) if $@;
    rmtree TMP_ZIP_DIR;
 }
-close FILE if $DEBUG;
-# $sth->finish;
-# CATS::DB::sql_disconnect;
 
 print_failed_zips;
 
+#-----------------------------------------------------------------
+#----------------------GET ALL TASKS FROM DB----------------------
+#-----------------------------------------------------------------
+CATS::DB::sql_connect;
+my $ary_ref = $dbh->selectall_arrayref('SELECT id, title FROM problems ORDER BY id');
+my %tasks = ();
+foreach (@$ary_ref) {
+   my ($id, $title) = @$_;
+   utf8::encode($title);
+   $title = sha1_hex($title);
+   if (defined $tasks{$title} && @{$tasks{$title}} > 0) {
+      push @{$tasks{$title}}, $id;
+   } else {
+      $tasks{$title} = [$id];
+   }
+}
+
+#-----------------------------------------------------------------
+#-------------------FIND ERRORS WITH ZIP CHAINS-------------------
+#-----------------------------------------------------------------
+CATS::DB::sql_disconnect;
+my %used_titles = ();
+sub set_id {
+   my ($desc, $amount) = @_;
+   my $has_error = 0;
+   $desc->{res_id} = $desc->{own_id} = undef;
+   $desc->{err} = [];
+   if (exists $tasks{$desc->{title}}) {
+      $used_titles{$desc->{title}} = 1;
+      $amount++;
+      $has_error = @{$tasks{$desc->{title}}} > 1;
+      push @{$desc->{err}}, 1 if $has_error; #"There is more than one id for $desc->{zip}"
+      $desc->{own_id} = $tasks{$desc->{title}}->[0];
+   } elsif (!exists $edges{$desc->{zip}}) {
+      $has_error = $amount > 0;
+      push @{$desc->{err}}, 4 if $has_error; #нету айди для последней задачи в цепочке
+   }
+   if (!exists $edges{$desc->{zip}}) {
+      $desc->{res_id} = $desc->{own_id};
+      $desc->{has_error} = $has_error || $amount > 1 || !$amount;
+      push @{$desc->{err}}, 2 if $amount > 1; #много входов из таблицы problems в цепочку истории
+      push @{$desc->{err}}, 3 if !$amount; #нету входов из таблицы задач
+   } else {
+      ($desc->{res_id}, $desc->{has_error}) = @{set_id($edges{$desc->{zip}}, $amount)};
+   }
+   return [$desc->{res_id}, $desc->{has_error}];
+}
+
+$_->{has_error} = set_id($_, 0)->[1] foreach @start_v;
+
+foreach (@start_v) {
+   next if !$_->{has_error};
+   my @errors = ();
+   my $lv = $_;
+   my $chain = $lv->{zip};
+   my $other_ids = $_->{own_id};
+   while (exists $edges{$lv->{zip}}) {
+      push @errors, "ERROR: There is more than one id for $lv->{zip}" if 1 ~~ @{$lv->{err}};
+      my $res_id = defined $lv->{res_id} ? $lv->{res_id} : -1;
+      $lv = $edges{$lv->{zip}};
+      $other_ids .= " $lv->{own_id}" if defined $lv->{own_id} && ($lv->{own_id} != $res_id);
+      $chain .= " => $lv->{zip}";
+   }
+   foreach my $err (@{$lv->{err}}) {
+      my $err_str;
+      if ($err == 1) {
+         $err_str = "ERROR: There is more than one id for $lv->{zip}";
+      } elsif ($err == 2) {
+         $err_str = "ERROR: More than one record in the database corresponds to the archives in the chain\nID'S:$other_ids";
+      } elsif ($err == 3) {
+         $err_str = "FATAL ERROR: There are no records in the database corresponding to the archives in the chain";
+      } elsif ($err == 4) {
+         $err_str = "FATAL ERROR: There is no record in the database for the last archive $lv->{zip} in the chain";
+      }
+      push @errors, $err_str if defined $err_str;
+   }
+   $, = "\n";
+   print ERROR_V_DEL . "\nCHAIN: $chain\n";
+   print @errors;
+   print "\n@{[ERROR_V_DEL]}\n";
+}
+
+foreach my $k (keys %tasks) {
+   next if exists $used_titles{$k};
+   print "ERROR: No corresponding archive for id $_\n" foreach @{$tasks{$k}};
+}
+print Dumper(@start_v);
+print "\n\n\n";
+# print Dumper %edges;
+# print "\n\n\n";
+# print Dumper %tasks;
+
+# print "\nZIP CHAINS\n";
+# my $amount = 0;
+# foreach (@start_v) {
+#    print "$_->{zip}";
+#    $amount++;
+#    my $zip = $_->{zip};
+#    while (exists $edges{$zip}) {
+#       print " => ";
+#       $zip = $edges{$zip}{zip};
+#       print "$zip";
+#       $amount++;
+#    }
+#    print "\n";
+# }
+
+# print "\nAMOUNT = " . $amount;
+# print "\n";
+
 exit;
+
+$, = "\n";
+print "=================\n";
+print %reverse_renamings;
+print "\n=================\n\n";
+
+# exit;
 
 my @log = $xml_repo->run(log => '--diff-filter=C', '-C', "-C@{[SIMILARITY_INDEX]}%", '--summary', '--format="% "');
 @log = reverse map {m/([0-9]+)\.xml => ([0-9]+)\.xml \(([0-9]+)%\)/; {old_name => $1, new_name => $2, sidx => $3}} grep {/^ copy/} @log;
