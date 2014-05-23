c2de5af7d70aaa699447c895a9367472f309d002
Mark Tertishniy <mtertishniy@gmail.com>
2014-05-23 14:38:21 +1100

    Добавление данных в бд вместе с хэшем и архивом

--- arch_ins.pl
+++ arch_ins.pl
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use lib CATS_DB;
 use CATS::DB;
+use CATS::BinaryFile;
 use File::stat;
 use File::Path;
 use XML::LibXML;
@@ -40,15 +41,15 @@ CATS::DB::sql_connect;
 
 $dbh->do('DELETE FROM problems');
 my $sth = $dbh->prepare(q~
-   INSERT INTO problems (id, title, contest_id, input_file, output_file) VALUES (?, ?, 1,'in','out')~);
+   INSERT INTO problems (id, title, zip_archive, hash, upload_date, contest_id, input_file, output_file) VALUES (?, ?, ?, ?, current_timestamp, 1,'in','out')~);
 
 my @titles = ();
-my @files = map {m|@{[PROBLEMS_DIR]}/(.*)|; $1} glob(PROBLEMS_DIR . '/*.zip');
+my @files = map {m|@{[PROBLEMS_DIR]}(.*)|; $1} glob(PROBLEMS_DIR . '*.zip');
 # my @files = sort{$files{$a} <=> $files{$b}} keys %files;
 
 foreach my $zip (@files) {
    my $xml_data;
-   my $zip_path = PROBLEMS_DIR . "/$zip";
+   my $zip_path = PROBLEMS_DIR . $zip;
    eval {
       eval {
          $xml_data = get_xml_from_zip($zip_path);
@@ -62,12 +63,19 @@ foreach my $zip (@files) {
       my $title = $atts->getNamedItem('title')->value if defined $atts->getNamedItem('title');
       utf8::encode($title);
       unless ($title ~~ @titles) {
+         my $data = '';
+         CATS::BinaryFile::load($zip_path, \$data) or error("open '$zip_path' failed: $!");
+         $zip =~ m/^problem_(.*).zip$/;
          $sth->bind_param(1, new_id);
          $sth->bind_param(2, $title);
+         $sth->bind_param(3, $data);
+         $sth->bind_param(4, $1);
          $sth->execute;
-         if ($title eq 'Космос для школьников' || $title eq "Testlib. Pascal: 1.9; C/C++: 0.3.3, 0.4.3;" || $title eq "New Pasal and C Testlibs: nums") {
+         if ($title eq 'Космос для школьников' || $title eq "Testlib. Pascal: 1.9; C/C++: 0.3.3, 0.4.3;") {
             $sth->bind_param(1, new_id);
             $sth->bind_param(2, $title);
+            $sth->bind_param(3, $data);
+            $sth->bind_param(4, $1);
             $sth->execute;
          }
          push @titles, $title;
